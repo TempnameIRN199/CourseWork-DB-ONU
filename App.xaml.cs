1b9350b4719186a68a93da2ebf2366650200beec
TempnameIRN199 <vityayuzbekov602@gmail.com>
2025-06-10 00:50:51 +0300

    Initial proj
    init proj n' some edit .gitignore

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CourseWork_DB_ONU
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
